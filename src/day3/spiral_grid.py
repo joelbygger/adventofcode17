2bb95350321d317d0b931a89f75d49c9e1aea512
Joel Säll <joel.sael@gmail.com>
2020-11-20 11:29:44 +0100

    Day 3 code pep adaptions.

--- src/day3/spiral_grid.py
+++ src/day3/spiral_grid.py
@@ -2,19 +2,20 @@ import collections
 import itertools
 import sys
 
+
 class SpiralGrid:
     def __init__(self):
-        self.dir_generator = self.direction_generator() # TODO make private?
+        self.dir_generator = self.direction_generator()  # TODO make private?
         self.neighbors = [
-            (-1, -1), (0 , -1), (1, -1), 
-            (-1,  0),           (1,  0), 
-            (-1,  1), (0,   1), (1,  1)]
+            (-1, -1), (0, -1), (1, -1),
+            (-1, 0), (1, 0),
+            (-1, 1), (0, 1), (1, 1)]
 
         self.start_pos = (1, 1)
         self.grid = collections.OrderedDict()
         self.grid[self.start_pos] = 1
 
-    def _add_point(self, increment : bool):
+    def _add_point(self, increment: bool):
         move_dir = next(self.dir_generator)
         last_point = next(reversed(self.grid))
         new_pos = (last_point[0] + move_dir[0], last_point[1] + move_dir[1])
@@ -33,23 +34,23 @@ class SpiralGrid:
     def direction_generator(self):
         steps_in_dir = 1
         steps_in_dir_increase = 0
-        
-        for dir in itertools.cycle([(1, 0), (0, -1), (-1 , 0), (0, 1)]): # Right, Up, Left, Down
+
+        for direction in itertools.cycle([(1, 0), (0, -1), (-1, 0), (0, 1)]):  # Right, Up, Left, Down
             for _ in range(steps_in_dir):
-                yield dir
+                yield direction
 
             steps_in_dir_increase += 1
             if steps_in_dir_increase >= 2:
                 steps_in_dir += 1
                 steps_in_dir_increase = 0
 
-    def create_grid(self, reach_value, increment : bool):
+    def create_grid(self, reach_value, increment: bool):
         while self.grid[next(reversed(self.grid))] < reach_value:
             self._add_point(increment)
 
     def get_manhattan_distance(self, to_value):
         # 1.                                    Get a list of all values
-        # 2.                                                             In the list, get the index that contains serched value.
+        # 2.                                                             List, get index that contains searched value.
         # 3. 
         # 4.             Get a list of all keys
         # 5.                                   In the list, get the key with the same index as searched for value
@@ -66,17 +67,19 @@ class SpiralGrid:
 if __name__ == "__main__":
     def task_1(search_value):
         grid = SpiralGrid()
-        grid.create_grid(search_value, increment = True)
-        
+        grid.create_grid(search_value, increment=True)
+
         dist = grid.get_manhattan_distance(search_value)
         print("Manhattan distance to value {} is {}".format(search_value, dist))
 
+
     def task_2(search_value):
         grid = SpiralGrid()
-        grid.create_grid(search_value, increment = False)
+        grid.create_grid(search_value, increment=False)
 
         val = grid.get_max_val()
         print("Number after incremented number: ", val)
+
 
     task_1(int(sys.argv[1]))
     task_2(int(sys.argv[1]))
--- src/day3/test_grid.py
+++ src/day3/test_grid.py
@@ -1,38 +1,46 @@
 import itertools
+
 import spiral_grid
 
 
 def test_get_directions():
     grid = spiral_grid.SpiralGrid()
-    assert list(itertools.islice(grid.direction_generator(), 25)) == [(1, 0), (0, -1), (-1, 0), (-1, 0), (0, 1), (0, 1), (1, 0), (1, 0), (1, 0), (0, -1), (0, -1), (0, -1), (-1, 0), (-1, 0), (-1, 0), (-1, 0), (0, 1), (0, 1), (0, 1), (0, 1), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0)]
+    assert list(itertools.islice(grid.direction_generator(), 25)) == [(1, 0), (0, -1), (-1, 0), (-1, 0), (0, 1), (0, 1),
+                                                                      (1, 0), (1, 0), (1, 0), (0, -1), (0, -1), (0, -1),
+                                                                      (-1, 0), (-1, 0), (-1, 0), (-1, 0), (0, 1),
+                                                                      (0, 1), (0, 1), (0, 1), (1, 0), (1, 0), (1, 0),
+                                                                      (1, 0), (1, 0)]
+
 
 def test_create_grid():
     grid = spiral_grid.SpiralGrid()
-    grid.create_grid(reach_value = 9, increment = True)
+    grid.create_grid(reach_value=9, increment=True)
     assert next(reversed(grid.grid)) == (2, 2)
-    assert grid.grid[1,1] == 1
-    assert grid.grid[0,0] == 5
-    assert grid.grid[2,2] == 9
+    assert grid.grid[1, 1] == 1
+    assert grid.grid[0, 0] == 5
+    assert grid.grid[2, 2] == 9
 
 
 def test_get_manhattan_distance_examples():
     grid = spiral_grid.SpiralGrid()
-    grid.create_grid(reach_value = 1024, increment = True)
+    grid.create_grid(reach_value=1024, increment=True)
 
     assert grid.get_manhattan_distance(1) == 0
     assert grid.get_manhattan_distance(12) == 3
     assert grid.get_manhattan_distance(23) == 2
     assert grid.get_manhattan_distance(1024) == 31
 
+
 def test_part_1_answer():
     grid = spiral_grid.SpiralGrid()
-    grid.create_grid(reach_value = 325489, increment = True)
+    grid.create_grid(reach_value=325489, increment=True)
 
     assert grid.get_manhattan_distance(325489) == 552
 
+
 def test_accumulate_examples():
     grid = spiral_grid.SpiralGrid()
-    grid.create_grid(reach_value = 54, increment = False)
+    grid.create_grid(reach_value=54, increment=False)
     assert grid.get_val_at_index(0) == 1
     assert grid.get_val_at_index(1) == 1
     assert grid.get_val_at_index(2) == 2
@@ -40,8 +48,9 @@ def test_accumulate_examples():
     assert grid.get_val_at_index(10) == 54
     assert grid.get_max_val() == 54
 
+
 def test_part_2_answer():
     grid = spiral_grid.SpiralGrid()
-    grid.create_grid(reach_value = 325489, increment = False)
+    grid.create_grid(reach_value=325489, increment=False)
 
     assert grid.get_max_val() == 330785
