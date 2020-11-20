b16df8857aa1d9c058377402c4bca11470c7cc8e
Joel Säll <joel.sael@gmail.com>
2020-11-20 14:40:39 +0100

    Day 4 task 2 done.

--- src/day4/pass_phrases.py
+++ src/day4/pass_phrases.py
@@ -6,7 +6,7 @@ import collections
 #        yield row
 
 def only_unique(words):
-    # A Set, with counts on duplicated words.
+    # A Set, with counter for duplicated words.
     counts = collections.Counter(words)
     # A list of tuples with most common words (I only ask for the most common one).
     # The tuple is like a dict, key : value. 
@@ -14,12 +14,15 @@ def only_unique(words):
     return counts.most_common(1)[0][1] <= 1
 
 
-def count_valid_readable(file):
+def count_valid_readable_code(file, anagram):
     i = 0
     for row in open(file):
         words = row.split()
+        if anagram:
+            words = [''.join(sorted(word)) for word in words]
         if only_unique(words):
             i += 1
+
     return i
 
 
@@ -28,8 +31,10 @@ def count_valid_list_comprehension(file):
 
 
 def main(file):
-    print("Duplicated passwords readable: ", count_valid_readable(file))
+    print("Duplicated passwords readable: ", count_valid_readable_code(file, anagram=False))
     print("Duplicated passwords list comprehension: ", count_valid_list_comprehension(file))
+
+    print("Duplicated passwords readable: ", count_valid_readable_code(file, anagram=True))
 
 
 if __name__ == "__main__":
--- src/day4/test_pass_phrases.py
+++ src/day4/test_pass_phrases.py
@@ -2,14 +2,22 @@ import pass_phrases
 
 
 def test_examples_task_1():
-    r1 = "aa bb cc dd ee"
-    r2 = "aa bb cc dd aa"
-    r3 = "aa bb cc dd aaa"
-    assert pass_phrases.only_unique(r1.split()) is True
-    assert pass_phrases.only_unique(r2.split()) is False
-    assert pass_phrases.only_unique(r3.split()) is True
+    e1 = "aa bb cc dd ee"
+    e2 = "aa bb cc dd aa"
+    e3 = "aa bb cc dd aaa"
+    assert pass_phrases.only_unique(e1.split()) is True
+    assert pass_phrases.only_unique(e2.split()) is False
+    assert pass_phrases.only_unique(e3.split()) is True
 
 
 def test_task_1():
-    assert 383 == pass_phrases.count_valid_readable("input")
+    assert 383 == pass_phrases.count_valid_readable_code("input", False)
     assert 383 == pass_phrases.count_valid_list_comprehension("input")
+
+
+def test_examples_task_2():
+    assert 3 == pass_phrases.count_valid_readable_code("input_examples_task2", True)
+
+
+def test_task_2():
+    assert 265 == pass_phrases.count_valid_readable_code("input", True)
